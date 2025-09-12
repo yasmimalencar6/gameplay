f49683fc969e712a2e1686714c58fc18936c451d
yasmimalencar-10 <yasmimleite1003@gmail.com>
2025-09-12 08:28:18 -0300

    okok
    coisas

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export function SigInin() {
   return (
