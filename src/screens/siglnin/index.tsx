d71d585e67e07cd352f6227a973ef9e4dab46d55
yasmimalencar-10 <yasmimleite1003@gmail.com>
2025-09-12 12:17:37 -0300

    assim tbm nao
    é vdd

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export function SigInin() {
+  
+  const [Text,SetText] = useState("sem nada")
   return (
     <View style={styles.container}>
       <Text> testar </Text>
+      <Text> ei louco ta ai :</Text>
+      <TextInput onChangeText={SetText} style={{width:200,height:50,borderWidth:2}}/>
+      <Text>Esse é show: {Text}</Text>
       <StatusBar style="auto" />
     </View>
   );
