790ca3125192aab25679b48129ee876c4ce60f5b
yasmimalencar-10 <yasmimleite1003@gmail.com>
2025-09-19 12:16:15 -0300

    ok
    okkk

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,26 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export function SigInin() {
+import {View,Image} from "react-native"
+import Illustration from "../../assets/illustration.png"
+export function SigInin (){
   
-  const [Text,SetText] = useState("sem nada")
   return (
-    <View style={styles.container}>
-      <Text> testar </Text>
-      <Text> ei louco ta ai :</Text>
-      <TextInput onChangeText={SetText} style={{width:200,height:50,borderWidth:2}}/>
-      <Text>Esse é show: {Text}</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Image source={Illustration}/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
