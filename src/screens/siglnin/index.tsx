43a2efa5477462e8997e57aac3d5ff3f161d8832
yasmimalencar-10 <yasmimleite1003@gmail.com>
2025-09-26 12:19:31 -0300

    gzg fd dhgd

--- src/screens/siglnin/index.tsx
+++ src/screens/siglnin/index.tsx
@@ -1,12 +1,26 @@
 
-import {View,Image} from "react-native"
+import {View,Image,Text} from "react-native"
 import Illustration from "../../assets/illustration.png"
+import { StylesSignIn } from "./styles";
+
 export function SigInin (){
   
   return (
-    <View>
-      <Image source={Illustration}/>
-    </View>
-  )
-}
+    <View style={StylesSignIn.container}>
+      <Image source={Illustration} style={StylesSignIn.image} resizeMode="stretch"/>
 
+      <View  style={StylesSignIn.content}>
+        <Text style={StylesSignIn.title}>
+         Organize suas {`\n`}
+         jogatinas {`\n`}
+         facilmente
+       </Text>
+       <Text style={StylesSignIn.subtitle}>
+        Crie grupos para jogar seus games {`\n`}
+        favoritos com seus amigos
+       </Text>
+      </View>
+      </View>
+
+        )
+       }
